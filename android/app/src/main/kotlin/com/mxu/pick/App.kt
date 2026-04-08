5ff2341225b753d9069904ff1e4da04bacd56b52
mia0x75 <miao.0x75@gmail.com>
2026-04-08 18:54:44 +0800

    perf: Android端预热FlutterEngine，进一步优化启动速度

---
+++ android/app/src/main/kotlin/com/mxu/pick/App.kt
@@ -0,0 +1,18 @@
+package com.mxu.pick
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+        val flutterEngine = FlutterEngine(this)
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint("lib/main.dart", "main")
+        )
+        FlutterEngineCache.getInstance().put("pick_engine", flutterEngine)
+    }
+}
--- android/app/src/main/kotlin/com/mxu/pick/MainActivity.kt
+++ android/app/src/main/kotlin/com/mxu/pick/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.mxu.pick
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun getFlutterEngine(): FlutterEngine {
+        return FlutterEngineCache.getInstance().get("pick_engine")!!
+    }
+}
