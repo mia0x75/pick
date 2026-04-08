b3a2925ab654d1f6fad138413e028143aa718eff
mia0x75 <miao.0x75@gmail.com>
2026-04-08 12:26:34 +0800

    fix: 优化import语句

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/services.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show Size;
+import 'package:flutter/services.dart' show LogicalKeyboardKey;
 
 class AppConstants {
   AppConstants._();
